2f164f8214f7b890c7fa3942e1962cbdbdc5f081
Daniel Ballerstein <daniel.ballerstein@zf.com>
2026-01-17 20:02:22 +0100

    Version 2

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app = FastAPI(title="OnePager API", version="0.5.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://DanielO815.github.io"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
